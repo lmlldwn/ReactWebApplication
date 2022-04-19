49e0682810cdef5a58fdf0da8a78948032d7273b
lmlldwn <lemuelaldwin.garcia@gcash.com>
2022-04-19 21:32:18 +0800

    added content page and styling

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import { useState } from 'react'
+
+import './Content.css'
+
 
 function Content() {
+  const initialState = {items: [], isLoaded: false}
+  const [state,setState] = useState(initialState);
+
+  const getJoke = () => {
+    fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
+      .then((res) => res.json())
+      .then((json) => {
+        setState({
+          items: json,
+          isLoaded: true
+        })
+      })
+  }
+
+  const DisplayJoke = () => {
+    return (
+        <div className='joke'>
+          <label id='joke-setup'>{state.items.setup} </label> <br/>
+          <label id='joke-delivery'>{state.items.delivery}</label> <br/>
+          <div className='joke-spinner'>
+            <label id='joke-emoji1'>🤣</label>
+            <label id='joke-emoji2'>🤣</label>
+            <label id='joke-emoji3'>🤣</label>
+          </div>
+        </div>
+    )
+  }
   return (
     <div>
-      <h1>Contents</h1>
+      <h1>Random Joke</h1>
+      {
+        !state.isLoaded ? (
+          <h1>Click the button below to generate a joke</h1>
+        ) : (
+          <DisplayJoke />
+        )
+      }
+      <button id='button-joke' onClick={getJoke}>Generate joke</button>
     </div>
   )
 }
