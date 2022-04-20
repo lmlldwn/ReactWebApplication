3ef0c49c5c6ab7ad3e3803ad5c9a8155cdd7b2b6
Lemuel Aldwin Garcia <lemuelaldwin.garcia@gcash.com>
2022-04-20 13:44:30 +0800

    Added information in about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
+
+function DisplayAbout() {
+  const author = {
+    name: "Lemuel Aldwin Garcia",
+    email: "lemuelaldwingarcia@gmail.com",
+    github: "www",
+    linkedin: "www"
+  }
+  return(
+    <div>
+      <div className='author'>
+        <label>Author: {author.name}</label>
+      </div>
+      <div className='contacts'>
+        <ul>
+          <li>Email: {author.email}</li>
+          <li>GitHub: {author.github}</li>
+          <li>LinkedIn: {author.linkedin}</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
 function About() {
   return (
     <div>
       <h1>About</h1>
+      <DisplayAbout />
     </div>
   )
 }
